ab203dfbe792eb6f0d9e7e03a1759375b09e511f
superern <ernandrewgregorio@gmail.com>
2025-03-06 00:36:36 -0600

    instantiable with different .env files using pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+// This file is used to configure the PM2 process manager
+module.exports = {
+  apps: [
+    {
+      name: "avr-nestjs-english",           // Unique name for this instance
+      script: "npm",                 // Command to execute
+      args: "run start",              // Arguments to pass to the script
+      env: {
+        ENV_FILE: ".env.english" // Path to the custom .env file
+      }
+    },
+    {
+      name: "avr-nestjs-chinese",           // Unique name for this instance
+      script: "npm",                 // Command to execute
+      args: "run start",              // Arguments to pass to the script
+      env: {
+        ENV_FILE: ".env.chinese" // Path to the custom .env file
+      }
+    }
+  ]
+};
