bfb36d578d9df30102d44a3c19bd3a20a6fa68fa
superern <ernandrewgregorio@gmail.com>
2025-03-17 09:36:39 -0500

    add thai language

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,6 +16,14 @@ module.exports = {
       env: {
         ENV_FILE: ".env.chinese" // Path to the custom .env file
       }
-    }
+    },
+    {
+      name: "avr-nestjs-thai",           // Unique name for this instance
+      script: "npm",                 // Command to execute
+      args: "run start",              // Arguments to pass to the script
+      env: {
+        ENV_FILE: ".env.thai" // Path to the custom .env file
+      }
+    },
   ]
 };
