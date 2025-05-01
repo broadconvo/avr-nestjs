da21cfbc01b4a9b2caec6e8af847a7148adb27f9
superern <ernandrewgregorio@gmail.com>
2025-05-01 01:58:10 -0500

    add invoice service add provider in audio module

--- src/audio/audio.module.ts
+++ src/audio/audio.module.ts
@@ -6,11 +6,17 @@ import { ConfigModule } from '@nestjs/config';
 import { ContactLookupController } from './controllers/contact-lookup.controller';
 import { ContactLookupService } from './services/contact-lookup.service';
 import { LangGraphModule } from '../langgraph/langgraph.module';
+import { InvoiceService } from '../langgraph/services/invoice.service';
 
 @Module({
   imports: [ConfigModule, LangGraphModule],
   controllers: [CallsController, ContactLookupController],
-  providers: [AudioSocketService, CallSessionService, ContactLookupService],
+  providers: [
+    AudioSocketService,
+    CallSessionService,
+    ContactLookupService,
+    InvoiceService,
+  ],
   exports: [AudioSocketService, CallSessionService, ContactLookupService],
 })
 export class AudioModule {}
